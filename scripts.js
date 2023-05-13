a00d0ff7a754d5b6704ef2fc64fb0eb5f8b9176d
datboi-coin <133405332+datboi-coin@users.noreply.github.com>
2023-05-14 00:46:17 +0100

    responsive update

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
-// var audio = document.getElementById("audioControl");
+// Loop music after 4seconds
+// const audio = document.getElementById('audioControl');
+// let delay = 4000; // 4 seconds
 
-// function playAudio() {
-//   audio.play();
-// }
+// audio.addEventListener('ended', function() {
+//   setTimeout(function() {
+//     audio.currentTime = 0;
+//     audio.play();
+//   }, delay);
+// }, false);
 
-// function pauseAudio() {
-//   audio.pause();
-// }
+// audio.play();
 
-const audio = document.getElementById('audioControl');
-let delay = 4000; // 4 seconds
-
-audio.addEventListener('ended', function() {
-  setTimeout(function() {
-    audio.currentTime = 0;
-    audio.play();
-  }, delay);
-}, false);
-
-audio.play();
+// Force auto play after 3seconds delay
+var audio = document.getElementById("audioControl");
+setTimeout(function() {
+  audio.play();
+}, 1500);
